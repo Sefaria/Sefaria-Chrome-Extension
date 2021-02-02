f4df452e6acd187f2e430306bea82ea80bed4bd5
Noah <noahssantacruz@gmail.com>
2021-02-02 12:14:23 +0200

    reenable dev build

--- create_release_zip.sh
+++ create_release_zip.sh
@@ -6,8 +6,8 @@ cp -R bundle_prod bundle
 zip -r -FS ./extension_outputs/extension.zip * -x '*node_modules*' 'js/*' '*\.DS_Store*' 'create_release_zip.sh' 'bundle_dev/*' 'bundle_prod/*' 'extension_outputs/*'
 zip -r -FS ./extension_outputs/extension_unobfuscated.zip * -x '*node_modules*' '*\.DS_Store*' 'create_release_zip.sh' 'bundle_dev/*' 'bundle_prod/*' 'extension_outputs/*'
 
-# rm -rf ./bundle
-# cp -R bundle_dev bundle
-# zip -r -FS ./extension_outputs/extension.dev.zip * -x '*node_modules*' 'js/*' '*\.DS_Store*' 'create_release_zip.sh' 'bundle_dev/*' 'bundle_prod/*' 'extension_outputs/*'
+rm -rf ./bundle
+cp -R bundle_dev bundle
+zip -r -FS ./extension_outputs/extension.dev.zip * -x '*node_modules*' 'js/*' '*\.DS_Store*' 'create_release_zip.sh' 'bundle_dev/*' 'bundle_prod/*' 'extension_outputs/*'
 
 rm -rf ./bundle
